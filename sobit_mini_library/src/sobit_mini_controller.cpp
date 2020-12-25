d4298f9576a6d0f07db6f3f78fb57c659af7b8b0
DaikiMin <dai.murakami98@gmail.com>
2020-12-25 16:55:58 +0900

    add sobit_mini_pybind.cpp

--- sobit_mini_library/src/sobit_mini_controller.cpp
+++ sobit_mini_library/src/sobit_mini_controller.cpp
@@ -1,7 +1,6 @@
 #include <sobit_mini_library/sobit_mini_controller.hpp>
 
 using namespace sobit;
-namespace py = pybind11;
 
 SobitMiniController::SobitMiniController( const std::string &name ) : SobitTurtlebotController( name ) {
     pub_body_control_ = nh_.advertise<trajectory_msgs::JointTrajectory>("/body_trajectory_controller/command", 1);
@@ -187,38 +186,4 @@ bool SobitMiniController::moveLeftArm ( const double shoulder_roll, const double
         ROS_ERROR("%s", ex.what());
         return false;
     }
-}
-
-
-PYBIND11_MODULE(sobit_mini_module, m) {
-    py::enum_<Joint>( m, "Joint" )
-        .value( "HEAD_PAN_JOINT", Joint::HEAD_PAN_JOINT )
-        .value( "HEAD_TILT_JOINT", Joint::HEAD_TILT_JOINT )
-        .value( "BODY_ROLL_JOINT", Joint::BODY_ROLL_JOINT )
-        .value( "RIGHT_SHOULDER_ROLL_JOINT", Joint::RIGHT_SHOULDER_ROLL_JOINT )
-        .value( "RIGHT_SHOULDER_FLEX_JOINT", Joint::RIGHT_SHOULDER_FLEX_JOINT )
-        .value( "RIGHT_ELBOW_ROLL_JOINT", Joint::RIGHT_ELBOW_ROLL_JOINT )
-        .value( "RIGHT_HAND_MOTOR_JOINT", Joint::RIGHT_HAND_MOTOR_JOINT )
-        .value( "LEFT_SHOULDER_ROLL_JOINT", Joint::LEFT_SHOULDER_ROLL_JOINT )
-        .value( "LEFT_SHOULDER_FLEX_JOINT", Joint::LEFT_SHOULDER_FLEX_JOINT )
-        .value( "LEFT_ELBOW_ROLL_JOINT", Joint::LEFT_ELBOW_ROLL_JOINT )
-        .value( "LEFT_HAND_MOTOR_JOINT", Joint::LEFT_HAND_MOTOR_JOINT )
-        .value( "JOINT_NUM", Joint::JOINT_NUM )
-        .export_values();
-    
-    py::class_<SobitTurtlebotController>(m, "SobitTurtlebotController")
-        .def( py::init< const std::string& >() )
-        .def( "controlWheelLinear", &SobitTurtlebotController::controlWheelLinear, "control Wheel Linear" )
-        .def( "controlWheelRotateRad", &SobitTurtlebotController::controlWheelRotateRad, "control Wheel Rotate Rad" )
-        .def( "controlWheelRotateDeg", &SobitTurtlebotController::controlWheelRotateDeg, "control Wheel Rotate Deg" );
-
-    py::class_<SobitMiniController, SobitTurtlebotController>(m, "SobitMiniController")
-        .def( py::init< const std::string& >() )
-        .def( "moveJoint", &SobitMiniController::moveJoint, "move Joint", 
-            py::arg("joint_num"), py::arg("rad"), py::arg("sec"), py::arg("is_sleep") = true )
-        .def( "moveHeadPanTilt", &SobitMiniController::moveHeadPanTilt, "move Head PanTilt", 
-            py::arg("pan_rad"), py::arg("tilt_rad"), py::arg("sec"), py::arg("is_sleep") = true )
-        .def( "movePose", &SobitMiniController::movePose, "move Pose" )
-        .def( "moveRightArm", &SobitMiniController::moveRightArm, "move Right Arm" )
-        .def( "moveLeftArm", &SobitMiniController::moveLeftArm, "move Left Arm" );
 }
