b573a7121823a5fb2cccfce4352506957f6cfc7d
Y-hanajima <sakuyuu2002@gmail.com>
2023-02-22 17:08:00 +0900

    Change package and Cmakelist

--- sobit_mini_library/example/test_pantilt_controll.py
+++ sobit_mini_library/example/test_pantilt_controll.py
@@ -16,10 +16,10 @@ def test():
 
     ang = -0.45
     # カメラパンチルトを動かす
-    mini_pantilt_ctr.moveJoint( Joint.HEAD_TILT_JOINT, ang, 2.0, True )
+    mini_pantilt_ctr.moveJoint( Joint.HEAD_PAN_JOINT, ang, 2.0, True )
     # r.sleep()
 
-    mini_pantilt_ctr.moveToPose( "initial_pose" )
+    # mini_pantilt_ctr.moveToPose( "initial_pose" )
 
 if __name__ == '__main__':
     try:
