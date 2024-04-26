28439eab3b63cebe4ad1bc9fc05c7d7da56d24e1
MrKeith99 <kvalentincardenas@hotmail.com>
2024-04-27 01:33:47 +0900

    [UPD] ros dependencies and tb2

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd $(pwd)/..
+echo "╔══╣ Setup: SOBIT MINI (STARTING) ╠══╗"
+
+
+# Keep track of the current directory
+DIR=`pwd`
+cd ..
 
 # git cloneしたいTeamSOBITSのROSパッケージを以下に記述
 ros_packages=(
@@ -17,40 +22,53 @@ for ((i = 0; i < ${#ros_packages[@]}; i++)) {
     git clone https://github.com/TeamSOBITS/${ros_packages[i]}.git
 }
 
-cd 
+# Setup Turtlebot2 (Kobuki) for ROS Noetic
+cd ${DIR}
+bash ../turtlebot2_on_noetic/turtlebot/setup_kobuki.sh
 
-# Setting Sound configure
-echo "pacmd load-module module-native-protocol-unix socket=/tmp/pulseaudio.socket &> /dev/null" >> ~/.bashrc
-echo "#!bin/bash
-touch /tmp/pulseaudio.client.conf
-echo \"default-server = unix:/tmp/pulseaudio.socket \n 
-      # Prevent a server running in the container \n 
-      autospawn = no \n 
-      daemon-binary = /bin/true \n
-      # Prevent the use of shared memory \n
-      enable-shm = false\" >> /tmp/pulseaudio.client.conf" | sudo tee /etc/profile.d/sound_setup.sh
-sudo bash /etc/profile.d/sound_setup.sh
+# Download ROS packages
+sudo apt-get update
+sudo apt-get install -y \
+    ros-$ROS_DISTRO-pybind11-catkin \
+    ros-$ROS_DISTRO-robot-state-publisher \
+    ros-$ROS_DISTRO-joint-state-controller \
+    ros-$ROS_DISTRO-joint-state-publisher \
+    ros-$ROS_DISTRO-joint-state-publisher-gui \
+    ros-$ROS_DISTRO-joint-limits-interface \
+    ros-$ROS_DISTRO-hardware-interface \
+    ros-$ROS_DISTRO-transmission-interface \
+    ros-$ROS_DISTRO-controller-interface \
+    ros-$ROS_DISTRO-controller-manager \
+    ros-$ROS_DISTRO-tf2 \
+    ros-$ROS_DISTRO-tf2-ros \
+    ros-$ROS_DISTRO-sensor-msgs \
+    ros-$ROS_DISTRO-trajectory-msgs \
+    ros-$ROS_DISTRO-geometry-msgs \
+    ros-$ROS_DISTRO-joy
 
-# Seting dynamixel USB1 (SOBIT PRO arm_pantilt)
-echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", SYMLINK+=\"input/dynamixel1\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/dynamixel1.rules
-# echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", ATTRS{serial}==\"E143\", SYMLINK+=\"input/dongle\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/dongle.rules
-# sudo /etc/init.d/udev reload
+# Setting up Dynamixel USB configuration (SOBIT MINI: Head and Arm Robot Mechanism)
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", SYMLINK+=\"input/dx_upper\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/dx_upper.rules
 
-# Seting dynamixel USB2 (SOBIT PRO wheel)
-echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6014\", SYMLINK+=\"input/dynamixel2\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/dynamixel2.rules
-# echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6014\", ATTRS{serial}==\"E148\", SYMLINK+=\"input/dxhub\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/dxhub.rules
-# sudo /etc/init.d/udev reload
-
-# Seting ps4_joy_control USB
+# Setting up PS4/PS3 Joystick USB configuration
 echo "KERNEL==\"uinput\", MODE=\"0666\"
       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"05c4\", MODE=\"0666\"
       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", KERNELS==\"0005:054C:05C4.*\", MODE=\"0666\"
       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"09cc\", MODE=\"0666\"
       KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", KERNELS==\"0005:054C:09CC.*\", MODE=\"0666\"" | sudo tee /etc/udev/rules.d/50-ds4drv.rules
-# sudo /etc/init.d/udev reload
 
+# Setting up kobuki rules
+echo "# On precise, for some reason, USER and GROUP are getting ignored.
+      # So setting mode = 0666 for now.
+      SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", ATTRS{serial}==\"kobuki*\", 
+      ATTR{device/latency_timer}=\"1\", MODE:=\"0666\", GROUP:=\"dialout\", SYMLINK+=\"input/kobuki\", KERNEL==\"ttyUSB*\"
+      # Bluetooth module (currently not supported and may have problems)
+      # SUBSYSTEM==\"tty\", ATTRS{address}==\"00:00:00:41:48:22\", MODE:=\"0666\", GROUP:=\"dialout\", SYMLINK+=\"input/kobuki\"" | sudo tee /etc/udev/rules.d/57-kobuki.rules
+
+# Reload udev rules
 sudo udevadm control --reload-rules
+
+# Trigger the new rules
 sudo udevadm trigger
 
-# USB Reload
-sudo /etc/init.d/udev reload
+
+echo "╚══╣ Setup: SOBIT MINI (FINISHED) ╠══╝"
