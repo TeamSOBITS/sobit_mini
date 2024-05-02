17cfbc4134a26d0d2c50e99c3606c16d526aa3e2
MrKeith99 <kvalentincardenas@hotmail.com>
2024-05-02 13:56:03 +0900

    [UPD] exec auto installer

--- install.sh
+++ install.sh
@@ -17,9 +17,16 @@ ros_packages=(
 )
 
 for ((i = 0; i < ${#ros_packages[@]}; i++)) {
-    # echo "array[$i] = ${array[i]}"
-    echo "${ros_packages[i]}"
+    echo "Clonning: ${ros_packages[i]}"
     git clone https://github.com/TeamSOBITS/${ros_packages[i]}.git
+
+    # Check if install.sh exists in each package
+    if [ -f ${ros_packages[i]}/install.sh ]; then
+        echo "Running install.sh in ${ros_packages[i]}."
+        cd ${ros_packages[i]}
+        bash install.sh
+        cd ..
+    fi
 }
 
 # Setup Turtlebot2 (Kobuki) for ROS Noetic
